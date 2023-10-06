f8e3dc257b9641ba625b86c0c3a273066e76b39f
Ugo Chinememerem <ugofranklin22@gmail.com>
2023-10-06 20:30:53 +0100

    main fixed

--- app/layout.js
+++ app/layout.js
@@ -1,15 +1,11 @@
 import "./globals.css";
-import { initializeApp } from "firebase/app";
+
 import { Inter } from "next/font/google";
 import Navbar from "@/components/navbar/page";
 import Footer from "@/components/Footer/page";
 import { useRouter } from "next/navigation";
 const inter = Inter({ subsets: ["latin"] });
 import Sidebar from "@/components/Sidebar/Sidebar";
-import {firebaseConfig} from "./server/config/firebaseConfig";
-
-initializeApp(firebaseConfig);
-
 
 export const metadata = {
   title: "SupportWizard",
