f9598fe88f2b9a6c8b748bfff078eb1239ecffb4
Ugo Chinememerem <ugofranklin22@gmail.com>
2023-10-07 02:57:28 +0100

    line

--- components/linechart/page.jsx
+++ components/linechart/page.jsx
@@ -1,42 +1,82 @@
+"use client";
 import React from "react";
+import { Line } from "react-chartjs-2";
 import {
-  ChartComponent,
-  SeriesCollectionDirective,
-  SeriesDirective,
-  Inject,
-  LineSeries,
-  DateTime,
-  Legend,
-  Tooltip,
-} from "@syncfusion/ej2-react-charts";
+  Chart,
+  LineController,
+  LineElement,
+  PointElement,
+  LinearScale,
+  Title,
+  CategoryScale,
+} from "chart.js";
 
-import Charthead from "../Charthead/page";
-import {
-  lineCustomSeries,
-  LinePrimaryXAxis,
-  LinePrimaryYAxis,
-} from "../../data";
+Chart.register(
+  LineController,
+  LineElement,
+  PointElement,
+  LinearScale,
+  Title,
+  CategoryScale
+);
+
 const LineChart = () => {
-  //   const { currentMode } = useStateContext();
+  const data = {
+    labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun"],
+    datasets: [
+      {
+        label: "Website Visitors",
+        data: [120, 220, 350, 280, 400, 320], // Replace with actual visitor data
+        fill: false,
+        borderColor: "#8A88FB",
+        borderWidth: 2,
+        pointRadius: 5,
+        pointBackgroundColor: "#8A88FB",
+      },
+    ],
+  };
+
+  const options = {
+    responsive: true, // Enable responsiveness
+    maintainAspectRatio: false, // Allow the aspect ratio to be adjusted
+    scales: {
+      y: {
+        type: "linear",
+        beginAtZero: true,
+        title: {
+          display: false,
+          text: "",
+          color: "#1E1E1E",
+          font: {
+            size: 14,
+            weight: "400",
+          },
+        },
+        ticks: {
+          color: "#45447e",
+        },
+      },
+      x: {
+        title: {
+          display: false,
+          text: "",
+          color: "#1E1E1E",
+          font: {
+            size: 14,
+            weight: "400",
+          },
+        },
+        ticks: {
+          color: "#45447e",
+        },
+      },
+    },
+  };
 
   return (
-    <ChartComponent
-      id="line-chart"
-      height="420px"
-      primaryXAxis={LinePrimaryXAxis}
-      primaryYAxis={LinePrimaryYAxis}
-      chartArea={{ border: { width: 0 } }}
-      tooltip={{ enable: true }}
-      // background="#282C34"
-      legendSettings={{ background: "white" }}
-    >
-      <Inject services={[LineSeries, DateTime, Legend, Tooltip]} />
-      <SeriesCollectionDirective>
-        {lineCustomSeries.map((item, index) => (
-          <SeriesDirective key={index} {...item} />
-        ))}
-      </SeriesCollectionDirective>
-    </ChartComponent>
+    <div className="w-full h-40 md:w-full md:h-96">
+      <Line data={data} options={options} />
+    </div>
   );
 };
 
