6605ab0bdf7b110c17b9618e24e1e9aa37b9ea7f
Ugo Chinememerem <ugofranklin22@gmail.com>
2023-10-07 02:38:47 +0100

    loginlogic

--- app/signin/page.jsx
+++ app/signin/page.jsx
@@ -1,45 +1,103 @@
-import React from "react";
-/* eslint-disable */
+"use client";
+import React, { useState } from "react";
+import { useRouter } from "next/navigation";
+import axios from "axios";
+
+const url = "https://chat-wizard.onrender.com/api/v1/users/login";
+
 const Signin = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [formData, setFormData] = useState({ email: "", password: "" });
+  const router = useRouter();
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    setIsLoading(true);
+
+    try {
+      const response = await axios.post(url, formData, {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+
+      if (response.status === 200) {
+        // Successful login, redirect to the dashboard
+        router.push("/dashboard");
+      } else {
+        console.log(response.statusText);
+        console.log(response.data); // Log the response data for debugging
+      }
+    } catch (error) {
+      if (error.response && error.response.status === 400) {
+        console.log("Validation error:", error.response.data); // Log server validation errors
+      } else {
+        console.error("An error occurred. Please try again later.");
+      }
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
   return (
-    <main className="flex items-center justify-center bg-horror min-h-screen  ">
-      {/* card container  */}
-      <div className="flex flex-col m-6 space-y-10 bg-wizard md:bg-white shadow-2xl  rounded-2xl md:flex-row md:space-y-0 md:m-0 ">
-        {" "}
-        {/* left side */}
-        <img
-          src="/images/sign.png"
-          alt="image"
-          className="w-[440px] hidden md:block"
-        />
-        {/* right side */}
-        <div className="p-6 md:p-12  bg-wizard  rounded-r-2xl ">
-          {/* Top content */}
-          <h2 className="mb-4 py-2 text-xl text-white font-bold">
-            {" "}
-            Welcome back
-          </h2>
-          {/* <p className="max-w-sm text-sm mb-12 font-light text-gray-600">
-            We're thrilled to have you join our Support Wizard community!
-          </p> */}
-          <label className="flex text-white py-3 ">Business email</label>
-          <input
-            type="text"
-            className="w-full mb-4  p-3 border border-gary-300 rounded-md placeholder:font-light focus:outline-none"
-            placeholder="name@work-email.com"
+    <>
+      <ToastContainer position="top-right" autoClose={3000} />
+      <main className="flex items-center justify-center bg-horror min-h-screen">
+        {/* card container */}
+        <div className="flex flex-col m-6 space-y-10 bg-wizard md:bg-white shadow-2xl rounded-2xl md:flex-row md:space-y-0 md:m-0">
+          {/* left side */}
+          <img
+            width={440}
+            height={440}
+            src="/images/sign.png"
+            alt="image"
+            className="w-[440px] hidden md:block"
           />
-          <label className="flex text-white py-3">Password</label>
-          <input
-            type="text"
-            className="w-full mb-12   p-3 border border-gary-300 rounded-md placeholder:font-light focus:outline-none"
-            placeholder="8 characters or more"
-          />
-          <button className="w-full p-3 text-white bg-horror rounded-full text-transform: capitalize hover:opacity-70 duration-200">
-            Login
-          </button>
+          {/* right side */}
+          <div className="p-6 md:p-12 bg-wizard rounded-r-2xl">
+            {/* Top content */}
+            <h2 className="mb-4 py-2 text-xl text-white font-bold">
+              Welcome back
+            </h2>
+            <form onSubmit={handleLogin}>
+              <label className="flex text-white py-3">Business email</label>
+              <input
+                type="email"
+                name="email"
+                value={formData.email}
+                onChange={handleInputChange}
+                className="w-full mb-4 p-3 border border-gray-300 rounded-md placeholder:font-light focus:outline-none"
+                placeholder="name@work-email.com"
+                required
+              />
+              <label className="flex text-white py-3">Password</label>
+              <input
+                type="password"
+                name="password"
+                value={formData.password}
+                onChange={handleInputChange}
+                className="w-full mb-12 p-3 border border-gray-300 rounded-md placeholder:font-light focus:outline-none"
+                placeholder="8 characters or more"
+                autoComplete="current-password"
+                required
+              />
+              <button
+                type="submit"
+                className="w-full p-3 text-white bg-horror rounded-full text-transform: capitalize hover:opacity-70 duration-200"
+                disabled={isLoading}
+              >
+                {isLoading ? "Logging in..." : " Login"}
+              </button>
+            </form>
+          </div>
         </div>
-      </div>
-    </main>
+      </main>
+    </>
   );
 };
 
--- app/signup/page.jsx
+++ app/signup/page.jsx
@@ -1,74 +1,150 @@
 "use client";
 import React, { useState } from "react";
-import { signUp } from "../server/authentication/signUp";
-/* eslint-disable */
-const Signup = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const handleSignUp = async () => {
-    try {
-      // Call the signUp function from your module
-      await signUp(email, password);
+import { useRouter } from "next/navigation";
+import axios from "axios";
+import Image from "next/image";
+import { toast, ToastContainer } from "react-toastify";
+import { FaEye, FaEyeSlash } from "react-icons/fa";
 
-      // Handle successful sign-up here, e.g., redirect to a dashboard or show a success message
-      console.log("User signed up successfully!");
+import "react-toastify/dist/ReactToastify.css";
+
+const url = "https://chat-wizard.onrender.com/api/v1/users";
+
+const Signup = () => {
+  const [showPassword, setShowPassword] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const [formData, setFormData] = useState({ email: "", password: "" });
+
+  const router = useRouter();
+
+  const togglePasswordVisibility = () => {
+    setShowPassword(!showPassword);
+  };
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSignUp = async (e) => {
+    e.preventDefault();
+
+    if (!formData.email || !formData.password) {
+      toast.error("Please fill all fields");
+      return;
+    }
+
+    setIsLoading(true);
+
+    try {
+      const response = await axios.post(url, formData, {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+
+      if (response.status === 201) {
+        toast.success("Successfully signed up", {
+          position: "top-right",
+          autoClose: 3000,
+          style: { background: "#7371D1", color: "white" },
+        });
+
+        router.push("/dashboard");
+
+        setFormData({ email: "", password: "" });
+      } else {
+        console.log(response.statusText);
+      }
     } catch (error) {
-      // Handle sign-up error here, e.g., display an error message
-      console.error("Sign-up error:", error.message);
+      if (error.response && error.response.status === 409) {
+        toast.error(
+          "Email address is already in use. Please use a different email."
+        );
+      } else {
+        toast.error("An error occurred. Please try again later.");
+      }
+      console.error(error);
+    } finally {
+      setIsLoading(false);
     }
   };
+
   return (
-    <main className="flex items-center justify-center bg-horror min-h-screen  ">
-      {/* card container  */}
-      <div className="flex flex-col m-6 space-y-10 bg-wizard md:bg-white shadow-2xl  rounded-2xl md:flex-row md:space-y-0 md:m-0 ">
-        {" "}
-        {/* left side */}
-        <img
-          src="/images/sign.png"
-          alt="image"
-          className="w-[440px] hidden md:block"
-        />
-        {/* right side */}
-        <div className="p-6 md:p-12  bg-wizard  rounded-r-2xl ">
-          {/* Top content */}
-          <h2 className="mb-4 py-2 text-xl text-white font-bold">
-            {" "}
-            Welcome to SupportWizard
-          </h2>
-          <p className="max-w-sm text-sm mb-12 font-light text-gray-600">
-            We're thrilled to have you join our Support Wizard community!
-          </p>
-          <label className="flex text-white py-3 ">Business email</label>
-          <input
-            type="text"
-            className="w-full mb-4  p-3 border border-gary-300 rounded-md placeholder:font-light focus:outline-none"
-            placeholder="name@work-email.com"
-            required
-            value={email}
-            onChange={(e) => {
-              setEmail(e.target.value);
-            }}
+    <>
+      <ToastContainer position="top-right" autoClose={3000} />
+      <main className="flex items-center justify-center bg-horror min-h-screen">
+        <div className="flex flex-col m-6 space-y-10 bg-wizard md:bg-white shadow-2xl rounded-2xl md:flex-row md:space-y-0 md:m-0">
+          <Image
+            width={440}
+            height={440}
+            src="/images/sign.png"
+            alt="image"
+            className="w-[440px] hidden md:block"
           />
-          <label className="flex text-white py-3">Password</label>
-          <input
-            type="password"
-            className="w-full mb-12   p-3 border border-gary-300 rounded-md placeholder:font-light focus:outline-none"
-            placeholder="8 characters or more"
-            value={password}
-            required
-            onChange={(e) => {
-              setPassword(e.target.value);
-            }}
-          />
-          <button
-            onClick={handleSignUp}
-            className="w-full p-3 text-white bg-horror rounded-full text-transform: capitalize hover:opacity-70 duration-200"
-          >
-            Create Account
-          </button>
+          <div className="p-6 md:p-12 bg-wizard rounded-r-2xl">
+            <h2 className="mb-4 py-2 text-xl text-white font-bold">
+              Welcome to SupportWizard
+            </h2>
+            <p className="max-w-sm text-sm mb-12 font-light text-gray-600">
+              We're thrilled to have you join our Support Wizard community!
+            </p>
+            <form onSubmit={handleSignUp}>
+              <div className="mb-4">
+                <label htmlFor="email" className="flex text-white py-3">
+                  Business email
+                </label>
+                <input
+                  type="text"
+                  id="email"
+                  name="email"
+                  className="w-full p-3 border border-gray-300 rounded-md placeholder:font-light focus:outline-none"
+                  placeholder="name@work-email.com"
+                  required
+                  value={formData.email}
+                  onChange={handleInputChange}
+                />
+              </div>
+              <div className="mb-12 relative">
+                <label htmlFor="password" className="flex text-white py-3">
+                  Password
+                </label>
+                <div className="relative">
+                  <input
+                    type={showPassword ? "text" : "password"}
+                    id="password"
+                    name="password"
+                    className="w-full p-3 border border-gray-300 rounded-md placeholder:font-light focus:outline-none"
+                    placeholder="8 characters or more"
+                    required
+                    autoComplete="current-password"
+                    value={formData.password}
+                    onChange={handleInputChange}
+                  />
+                  <span
+                    className="cursor-pointer absolute top-0 right-0 mt-3 mr-3"
+                    onClick={togglePasswordVisibility}
+                  >
+                    {showPassword ? (
+                      <FaEyeSlash className="text-gray-500" />
+                    ) : (
+                      <FaEye className="text-gray-500" />
+                    )}
+                  </span>
+                </div>
+              </div>
+
+              <button
+                type="submit"
+                className="w-full p-3 text-white bg-horror rounded-full text-transform: capitalize hover:opacity-70 duration-200"
+              >
+                {isLoading ? "Creating account..." : "Create account"}
+              </button>
+            </form>
+          </div>
         </div>
-      </div>
-    </main>
+      </main>
+    </>
   );
 };
 
